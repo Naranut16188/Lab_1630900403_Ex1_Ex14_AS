8a69901787b6f309e0a90a7434c66b4eeb070750
Naranut16188 <90574866+Naranut16188@users.noreply.github.com>
2021-12-14 00:24:52 +0700

    Add files via upload

---
+++ ex4/js_ex4_2.js
@@ -0,0 +1,5 @@
+//JavaScript by Thanyaphon riodaphan 1630900403
+//Ex4.2: JavaScript Numbers and Strings
+document.getElementById("ex4-2-1").innerHTML = 93.69; //Number
+document.getElementById("ex4-2-2").innerHTML = "Thanyaphon"; //String
+document.getElementById("ex4-2-3").innerHTML = "Thanyaphon score: " + 93.69; //Number + String
